f69f86e8a7c6c2c88131eb713904d5ddb3f550ce
Shamal Jadhav <161221052+Jadhav-Shamal-123@users.noreply.github.com>
2026-07-20 21:22:36 +0530

    Add files via upload

---
+++ Week_5/Angular/Hands_On_5/pages/reactive-enrollment-form/reactive-enrollment-form.spec.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReactiveEnrollmentFormComponent } from './reactive-enrollment-form';
+
+describe('ReactiveEnrollmentFormComponent', () => {
+
+  let component: ReactiveEnrollmentFormComponent;
+  let fixture: ComponentFixture<ReactiveEnrollmentFormComponent>;
+
+  beforeEach(async () => {
+
+    await TestBed.configureTestingModule({
+      imports: [
+        ReactiveEnrollmentFormComponent,
+        CommonModule
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(
+      ReactiveEnrollmentFormComponent
+    );
+
+    component = fixture.componentInstance;
+
+    await fixture.whenStable();
+
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+});
