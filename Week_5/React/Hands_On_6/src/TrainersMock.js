ec56c31da8b9c6ade43f41d73800b6faf4683f2a
Shamal Jadhav <161221052+Jadhav-Shamal-123@users.noreply.github.com>
2026-07-21 23:56:12 +0530

    Add files via upload

---
+++ Week_5/React/Hands_On_6/src/TrainersMock.js
@@ -0,0 +1,10 @@
+import Trainer from "./trainer";
+
+const trainers = [
+    new Trainer(1, "Rahul Sharma", "rahul@gmail.com", "9876543210", "React", "React, JavaScript"),
+    new Trainer(2, "Amit Patil", "amit@gmail.com", "9876543211", "Angular", "Angular, TypeScript"),
+    new Trainer(3, "Priya Singh", "priya@gmail.com", "9876543212", "Java", "Spring Boot"),
+    new Trainer(4, "Neha Joshi", "neha@gmail.com", "9876543213", ".NET", "ASP.NET Core")
+];
+
+export default trainers;
