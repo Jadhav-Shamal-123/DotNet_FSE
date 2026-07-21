0ec2be3811c30ea4f0398027cdf2bfd8d5c0bce7
Shamal Jadhav <161221052+Jadhav-Shamal-123@users.noreply.github.com>
2026-07-22 00:16:18 +0530

    Add files via upload

---
+++ Week_5/React/Hands_On_7/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OnlineShopping from './components/OnlineShopping';
+
+function App() {
+  return (
+    <div>
+      <OnlineShopping />
+    </div>
+  );
+}
+
+export default App;
